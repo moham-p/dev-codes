f7a1819dfda448ec7a03ac3efb96ebd35ae1b4a7
Moham Pandi <moham.pandi@envistaco.com>
2024-11-12 08:21:02 +0200

    add security config and OrderController

--- spring/security/src/main/java/dev/moham/ss/config/SecurityConfig.java
+++ spring/security/src/main/java/dev/moham/ss/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package dev.moham.ss.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
