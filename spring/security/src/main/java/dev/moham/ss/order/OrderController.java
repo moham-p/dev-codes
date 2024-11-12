f0935c69db578f6cb17ad73756f0354e7c4079cb
Moham Pandi <moham.pandi@envistaco.com>
2024-11-12 08:20:25 +0200

    add security config and OrderController

---
+++ spring/security/src/main/java/dev/moham/ss/order/OrderController.java
@@ -0,0 +1,9 @@
+package dev.moham.ss.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+}
